3ad63b085a5b8b675656d754e8d4d345caac2a0f
dimostant <dimos.2000@hotmail.com>
2024-08-23 05:42:58 +0300

    improved form/edit page

--- resources/views/Customers/customers.blade.php
+++ resources/views/Customers/customers.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('content')
 
-<div class="relative overflow-x-auto">
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -17,12 +17,13 @@
                 <th scope="col" class="px-6 py-3">
                     Company
                 </th>
-                <th></th>
-                <th></th>
+                <th scope="col" class="px-6 py-3 text-center">
+                    Actions
+                </th>
             </tr>
         </thead>
         <tbody>
-            @foreach ( $customers as $customer) 
+            @foreach ($customers as $customer) 
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     {{ $customer->email }}
@@ -33,26 +34,25 @@
                 <td class="px-6 py-4">
                     {{ $customer->phone_number }}
                 </td>
-                <td>
-                    {{ $customer->company}}
+                <td class="px-6 py-4">
+                    {{ $customer->company }}
                 </td>
-                {{-- workaround --}}
-                <form action="{{ route('form.edit', $customer->id)}}" method="POST">
-                    @method('GET')
-                    <td>
-                    <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Edit</button>
-                    </td>
-                </form>
-                <form action="{{ route('form.edit.destroy', $customer->id)}}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <td>
-                    <button type="submit" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                <td class="px-6 py-4 text-center">
+                    <div class="flex justify-center space-x-2">
+                        <form action="{{ route('form.edit', $customer->id) }}" method="GET">
+                            <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Edit</button>
+                        </form>
+                        <form action="{{ route('form.edit.destroy', $customer->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                        </form>
+                    </div>
                 </td>
-                </form>
             </tr>
             @endforeach
         </tbody>
     </table>
 </div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -23,18 +23,18 @@ Route::get('/pricing', function () {
     return view('pricing');
 });
 
-Route::get('/actions',function () {
+Route::get('/actions', function () {
     return view('actions');
 });
 
-Route::get('/notfound',function () {
+Route::get('/notfound', function () {
     return view('notfound');
 });
 
 Route::get('/form', [CustomerController::class, 'index']);
 Route::patch('/form', [CustomerController::class, 'store']);
 
-Route::get('/form/edit',[CustomerController::class, 'index'])->name('form.editall');
+Route::get('/form/edit', [CustomerController::class, 'index'])->name('form.editall');
 
 Route::get('/form/edit/{customer}', [CustomerController::class, 'edit'])->name('form.edit');
 Route::put('/form/edit/{customer}', [CustomerController::class, 'update'])->name('form.update');
